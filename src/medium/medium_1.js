644ecf3ed9c6d30143fbf04f524f53916c884f15
hackerrank <me@hackerrank.com>
2021-11-02 22:19:46 -0400

    last try

--- src/medium/medium_1.js
+++ src/medium/medium_1.js
@@ -88,13 +88,4 @@ export function getStatistics(array) {
     return finobj;
 
 }
-export function variance(array, mean) {
-    return array.map(function (sample) {
-        return Math.pow(mean - sample, 2);
-    })
-        .reduce(function sum(m, v) {
-            m += v;
-            return m;
-        }, 0) / array.length;
-}
 
