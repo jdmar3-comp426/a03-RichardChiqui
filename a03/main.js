cc0d99765c27a7cd50419b7fd818a0a1d58c4a01
hackerrank <me@hackerrank.com>
2021-11-02 23:52:23 -0400

    did it

--- a03/main.js
+++ a03/main.js
@@ -3,8 +3,9 @@
 import{getIncreasingArray} from "../src/mild/mild_1.js";
 import { getSum } from "../src/medium/medium_1.js";
 import { repeatDemo } from "../src/spicy/spicy_9.js";
+import {tenTimesFifty} from "../src/spicy/spicy_9.js";
 
-console.log(repeatDemo());
+console.log(tenTimesFifty());
 
 
 
