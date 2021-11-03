607ca1c1725de9d28ef5f6f6ba8fcc9895f191b5
hackerrank <me@hackerrank.com>
2021-11-02 23:28:42 -0400

    removed objects

--- src/medium/medium_3.js
+++ src/medium/medium_3.js
@@ -22,7 +22,7 @@ export function searchHighPower(car_data, minHorsepower, minTorque) {
 
     for(let i=0; i<car_data.length;i++){
         if(car_data[i].horsepower >= minHorsepower && car_data[i].torque >= minTorque){
-            arr[i] = new Object();
+
             arr.push(car_data[i]);
 
         }
